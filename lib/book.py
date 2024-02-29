2261bee46fafdd0fc043c7680f095e50e40c40a6
dkiriinya <dkiriinya06sc@gmail.com>
2024-02-29 13:19:05 +0300

    putting it all together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python3
+import imdb
 
 class Book:
-    pass
+    def __init__(self , title , page_count):
+        self.title = title
+        self.page_count = page_count
+        
+    
+    @property
+    def title(self):
+        # The title property
+        return self._title
+    
+    @title.setter
+    def title(self,title):
+        self._title = title
+        
+        
+    @property
+    def page_count(self):
+        #The Page COunt property
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self,page_count):
+        if isinstance(page_count,int):
+            self._page_count = page_count
+        else:
+            print(
+                "page_count must be an integer"
+            )
+    
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        
+        
+
+        
+        
     
         
